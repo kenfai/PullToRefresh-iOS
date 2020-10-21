3237cdd389f1b12345cb78e619031b550ca2f252
KenFai <ngkenfai@gmail.com>
2020-10-21 11:57:40 +0800

    Initial Commit

---
+++ PullToRefresh/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PullToRefresh
+//
+//  Created by Ginger on 21/10/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
